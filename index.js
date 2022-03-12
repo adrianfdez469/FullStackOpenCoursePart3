30bc0a59ff7718552ddcbd9043d6d2f7188022a9
Adrian <adrianfdez469@gmail.com>
2022-03-12 13:12:12 -0500

    Changing listen port

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ app.post('/api/persons', (req, resp) => {
 
 
 
-const PORT = 3001 
+const PORT = process.env.PORT || 3001 
 app.listen(PORT, () => {
   console.log(`Express backend started at port ${PORT}`);
 })
